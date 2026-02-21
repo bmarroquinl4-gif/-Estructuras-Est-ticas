7a1ecd5d65f2465c84dadc30f99be18bd45a0482
bmarroquinl4-gif <bmarroquinl4@miumg.edu.gt>
2026-02-20 22:40:45 -0600

    Add files via upload

---
+++ EncriptadorASCII/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace EncriptadorASCII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCadena = new System.Windows.Forms.TextBox();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese una cadena: ";
+            // 
+            // txtCadena
+            // 
+            this.txtCadena.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtCadena.Location = new System.Drawing.Point(71, 87);
+            this.txtCadena.Name = "txtCadena";
+            this.txtCadena.Size = new System.Drawing.Size(251, 22);
+            this.txtCadena.TabIndex = 1;
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.Location = new System.Drawing.Point(246, 174);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(171, 23);
+            this.btnEncriptar.TabIndex = 2;
+            this.btnEncriptar.Text = "Encriptar";
+            this.btnEncriptar.UseVisualStyleBackColor = true;
+            this.btnEncriptar.Click += new System.EventHandler(this.btnEncriptar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblResultado.Location = new System.Drawing.Point(120, 292);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 16);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.txtCadena);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCadena;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ EncriptadorASCII/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+
+namespace EncriptadorASCII
+{
+
+    //Clase principal del proyecto 
+    public partial class Form1 : Form
+    {
+
+        //es el metodo constructor
+        public Form1()
+
+        {
+
+            //metodo que inicializa los componentes graficos
+            InitializeComponent();
+        }
+
+
+        // evento que se crea cuando se hace clic al boton 
+        private void btnEncriptar_Click(object sender, EventArgs e)
+        {
+            // obtiene el texto que se escribe
+            string cadena = txtCadena.Text;
+
+            // Validar que no esté vacía
+            if (string.IsNullOrWhiteSpace(cadena))
+
+
+            {
+                // da una adbertencia si no se ingresa una cadena 
+                MessageBox.Show("Debe ingresar una cadena.");
+                return;
+            }
+
+            string asciiOriginal = "";
+            string asciiEncriptado = "";
+
+            // bucle que recorre cada caracter dela cadena
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                // convierte a cadena a su codigo ascii
+                int ascii = (int)cadena[i];
+
+                //hace la encriptacion y le suma 3
+                int encriptado = ascii + 3;
+
+                asciiOriginal += ascii + " ";
+                asciiEncriptado += encriptado + " ";
+            }
+            // muestra el resultado 
+
+            lblResultado.Text =
+                "ASCII: " + asciiOriginal + Environment.NewLine +
+                "ASCII + 3: " + asciiEncriptado;
+        }
+    }
+}
